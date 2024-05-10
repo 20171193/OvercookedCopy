4b1bcf5da8be6e7e19f8ff742f37739d47f1f944
bbiyakbbiyak <wonwon1028@naver.com>
2024-05-10 09:54:57 +0900

    feat : Chatacter Rotate Function Added

--- Assets/PlayerMover.cs
+++ Assets/PlayerMover.cs
@@ -1,22 +1,14 @@
-using System.Runtime.InteropServices;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 namespace KIMJAEWON
 {
     public class PlayerMover : MonoBehaviour
     {
         public float moveSpeed = 5f;
+        public float rotationSpeed = 10f; // 회전 속도를 조절할 변수 추가
 
-        float xRotation;
-        float yRotation;
-
-        [SerializeField]
         private Rigidbody rb;
-
-        [SerializeField]
         private Vector2 moveInput;
 
         void Start()
@@ -26,44 +18,30 @@ namespace KIMJAEWON
 
         void FixedUpdate()
         {
+            Move();
+            Rotate();
+        }
+
+        void Move()
+        {
             Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
             Vector3 moveVelocity = moveDirection * moveSpeed;
             rb.velocity = moveVelocity;
+        }
 
-            //Rotate();
-      
-
+        void Rotate()
+        {
+            if (moveInput != Vector2.zero)
+            {
+                float targetAngle = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg;
+                Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
         }
 
         public void OnMove(InputValue value)
         {
             moveInput = value.Get<Vector2>();
         }
-
-        //public void Rotate()
-        //{
-        //    float Horizontal = Input.GetAxis("Horizontal");
-        //    float Vertical = Input.GetAxis("Vertical");
-
-        //    // Calculate rotation based on input
-        //    Vector3 rotation = new Vector3(Vertical, 0f, -Horizontal) * Time.deltaTime * moveSpeed;
-
-        //    // Apply rotation to the object
-        //    transform.RotateAround(transform.position, Vector3.up, rotation.y);
-        //    transform.RotateAround(transform.position, transform.right, rotation.x);
-        //}
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
