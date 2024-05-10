91a9bf81dbd621307b78540019cf8bd7a9062bb1
심재천 <shimnaldo98@gmail.com>
2024-05-10 10:40:28 +0900

    feat : 메인 페널 작업

--- Assets/PG_ShimJaechun/02. Scripts/Main/MainPanelController.cs
+++ Assets/PG_ShimJaechun/02. Scripts/Main/MainPanelController.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainPanelController : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject mainPanel;           // 메인 선택 패널
+    public enum PanelType { Login, Main, Campagin, ChefSelect, Option}
 
     [SerializeField]
-    private GameObject campaginSelectPanel; // 캠페인 선택 패널
+    private TextMeshProUGUI debbugingText;      // 디버깅용 텍스트
 
     [SerializeField]
-    private GameObject characterSelectPanel;   // 
+    private GameObject mainPanel;               // 메인 선택 패널
 
+    [SerializeField]
+    private GameObject campaginSelectPanel;     // 캠페인 선택 패널
 
+    [SerializeField]
+    private GameObject chefSelectPanel;         // 요리사 선택 패널
+
+    private void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnClickCampaginButton()
+    {
+
+    }
+    public void OnClick
 
 
     public void OnActivePanel()
