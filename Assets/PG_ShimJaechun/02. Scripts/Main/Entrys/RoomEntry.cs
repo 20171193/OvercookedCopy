a14c107565218e743a5c93f45fa6263eba1f8975
심재천 <shimnaldo98@gmail.com>
2024-05-10 17:34:23 +0900

    feat : 로비 네트워크 작업

---
+++ Assets/PG_ShimJaechun/02. Scripts/Main/Entrys/RoomEntry.cs
@@ -0,0 +1,44 @@
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jc
+{
+    public class RoomEntry : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI roomName;           // 방 정보
+        [SerializeField]
+        private TextMeshProUGUI currentPlayer;      // 플레이어 정보
+        [SerializeField]
+        private Button joinRoomButton;              // 입장 버튼
+
+        private RoomInfo roomInfo;
+
+        private void OnEnable()
+        {
+
+        }
+
+        public void SetRoomInfo(RoomInfo roomInfo)
+        {
+            // RoomEntry의 RoomInfo를 할당
+            this.roomInfo = roomInfo;
+            // 방 이름
+            roomName.text = roomInfo.Name;
+            // 방에 참여한 플레이어 수
+            currentPlayer.text = $"{roomInfo.PlayerCount} / {roomInfo.MaxPlayers}";
+            // Join 버튼 활성화 여부
+            joinRoomButton.interactable = roomInfo.PlayerCount < roomInfo.MaxPlayers;
+        }
+
+        public void JoinRoom()
+        {
+            // 룸 입장
+            PhotonNetwork.LeaveLobby();
+            PhotonNetwork.JoinRoom(roomInfo.Name);
+        }
+    }
+}
