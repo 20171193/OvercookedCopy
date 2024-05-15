abe7ebb36c160ea095e710d7182588929d6407ff
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-05-15 23:27:22 +0900

    feat : 외곽선 쉐이더 추가

--- Assets/PG_ImJoonhi/02. Scripts/Debug/Temp_Mover.cs
+++ Assets/PG_ImJoonhi/02. Scripts/Debug/Temp_Mover.cs
@@ -7,7 +7,7 @@ namespace JH
     {
         private Vector3 moveDir;
         private Vector3 rotation;
-        public Rigidbody rigidbody;
+        public Rigidbody rigidbody_Temp;
 
         public int movePower;
         public int maxSpeed;
@@ -21,10 +21,10 @@ namespace JH
         private void Move()
         {
             Vector3 forceDir = transform.forward * moveDir.z;
-            rigidbody.AddForce(forceDir * movePower, ForceMode.Force);
-            if (rigidbody.velocity.magnitude > maxSpeed)
+            rigidbody_Temp.AddForce(forceDir * movePower, ForceMode.Force);
+            if (rigidbody_Temp.velocity.magnitude > maxSpeed)
             {
-                rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;  // 최대속도에서 속도제한
+                rigidbody_Temp.velocity = rigidbody_Temp.velocity.normalized * maxSpeed;  // 최대속도에서 속도제한
             }
         }
 
