e6421a923514310162b9419f650f75177ccf2d3c
임준희 <imjoonhi@gmail.com>
2024-05-16 17:22:58 +0900

    feat : 후라이팬 시간경과 제외한 기본기능 구현 밑 프리팹화

--- Assets/PG_ImJoonhi/02. Scripts/Pan.cs
+++ Assets/PG_ImJoonhi/02. Scripts/Pan.cs
@@ -1,18 +1,59 @@
+using JH;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pan : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Cook State")]
+    public bool OnCooker;
+    public bool Cooking;
+    public float CookSpeed;
+
+    [SerializeField]
+    [Range(0.0f, 10.0f)]
+    private float progress;
+
+    [Header("Potints")]
+    [SerializeField] GameObject PanPoint;
+
+    [Header("Debug")]
+    [SerializeField] IngredientsObject DebugCookingObject;
+
+    private IngredientsObject CookingObject;
+
+    public bool IngredientIN(IngredientsObject ingredient)
     {
-        
+        if(!Cooking && ingredient.ingredientsData.Paned != null)
+        {
+            OnPan(ingredient);
+            return true;
+        }
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPan(IngredientsObject ingredient)
     {
-        
+        Cooking = true;
+        CookingObject = ingredient;
+        CookingObject.transform.position = PanPoint.transform.position;
+        CookingObject.transform.SetParent(PanPoint.transform, true);
     }
+
+
+    #region Debug
+#if UNITY_EDITOR
+    [ContextMenu("[Debug]DebugIngredient Cook")]
+    public void DebugCook()
+    {
+        IngredientIN(DebugCookingObject);
+    }
+
+    [ContextMenu("[Debug]Done")]
+    public void DebugDone()
+    {
+        CookingObject.PanHeated();
+    }
+#endif
+    #endregion
 }
