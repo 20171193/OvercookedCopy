127d515c99ec38216e78a275f078f33c8b0ea5bb
bbiyakbbiyak <wonwon1028@naver.com>
2024-05-16 17:34:01 +0900

    feat : 플레이어 아이템 집기 작업

--- Assets/PG_KimJaewon/Scripts/ObjectTrigger.cs
+++ Assets/PG_KimJaewon/Scripts/ObjectTrigger.cs
@@ -2,85 +2,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectTrigger : MonoBehaviour
+namespace KIMJAEWON
 {
-    private Material originTile;
-    [SerializeField]
-    private Material changeTile;
-    private Table nearestTable;
-
-    private List<Table> tableList = new List<Table>(); 
-
-    public Color color;
-
-    private void Update()
+    public class ObjectTrigger : MonoBehaviour
     {
-        CalculateDistance(); 
-    }
+        private Material originTile;
+        [SerializeField]
+        private Material changeTile;
+        private Table nearestTable;
 
-    private void CalculateDistance()
-    {
-        if (tableList.Count < 1)
-        {
-            return;
-        }
-        else if (tableList.Count == 1)
-        {
-            if(nearestTable != null)
-            {
-                nearestTable.ExitPlayer();
-            }            
-            nearestTable = tableList[0];
-        }
-        else
-        {
-            float minDist = -1f;
-            for (int i = 0; i < tableList.Count; i++)
-            {
-                Table table = tableList[i];
-                float dist = Vector3.Distance(transform.position, table.transform.position);
+        private List<Table> tableList = new List<Table>();
 
-                if (minDist == -1f)
+        public Color color;
+
+        private void Update()
+        {
+            CalculateDistance();
+        }
+
+        private void CalculateDistance()
+        {
+            if (tableList.Count < 1)
+            {
+                return;
+            }
+            else if (tableList.Count == 1)
+            {
+                if (nearestTable != null)
                 {
-                    minDist = dist;
-                    if (nearestTable != null)
-                    {
-                        nearestTable.ExitPlayer();
-                    }
-                    nearestTable = table;
+                    nearestTable.ExitPlayer();
                 }
-                else if (dist < minDist)
+                nearestTable = tableList[0];
+            }
+            else
+            {
+                float minDist = -1f;
+                for (int i = 0; i < tableList.Count; i++)
                 {
-                    minDist = dist;
-                    if (nearestTable != null)
+                    Table table = tableList[i];
+                    float dist = Vector3.Distance(transform.position, table.transform.position);
+
+                    if (minDist == -1f)
                     {
-                        nearestTable.ExitPlayer();
+                        minDist = dist;
+                        if (nearestTable != null)
+                        {
+                            nearestTable.ExitPlayer();
+                        }
+                        nearestTable = table;
                     }
-                    nearestTable = table;
+                    else if (dist < minDist)
+                    {
+                        minDist = dist;
+                        if (nearestTable != null)
+                        {
+                            nearestTable.ExitPlayer();
+                        }
+                        nearestTable = table;
+                    }
                 }
             }
+            nearestTable.EnterPlayer();
         }
-        nearestTable.EnterPlayer();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // 충돌한 오브젝트의 태그가 Table이 맞다면
-        if (other.CompareTag("Table"))
+        private void OnTriggerEnter(Collider other)
         {
-            // 테이블 스크립트를 가져온다.
-            Table table = other.GetComponent<Table>();
-            tableList.Add(table);
+            // 충돌한 오브젝트의 태그가 Table이 맞다면
+            if (other.CompareTag("Table"))
+            {
+                // 테이블 스크립트를 가져온다.
+                Table table = other.GetComponent<Table>();
+                tableList.Add(table);
+            }
         }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Table"))
+        private void OnTriggerExit(Collider other)
         {
-            Table table = other.GetComponent<Table>();
-            table.ExitPlayer();
+            if (other.CompareTag("Table"))
+            {
+                Table table = other.GetComponent<Table>();
+                table.ExitPlayer();
 
-            tableList.Remove(table);
+                tableList.Remove(table);
+            }
         }
     }
 }
