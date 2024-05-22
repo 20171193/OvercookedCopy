1146b9146a790ff2d53a46d0a84ce86a1a5d3aeb
심재천 <shimnaldo98@gmail.com>
2024-05-22 21:40:46 +0900

    feat : 셰프모델 추가 및 메인 씬 버스모델 적용 (에일리언, 남성, 곰)

--- Assets/PG_ShimJaechun/02. Scripts/00. Title/TitleController.cs
+++ Assets/PG_ShimJaechun/02. Scripts/00. Title/TitleController.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 namespace Jc
 {
     public class TitleController : MonoBehaviour
     {
         private bool isLoadScene;
 
+        [SerializeField]
+        private float loadTime;             // 씬 전환 로딩시간
+
+        [SerializeField]
+        private Image loadingFadeImage;     // 로딩 페이드인 이미지
+        [SerializeField]
+        private GameObject loadingGroup;    // 로딩 그룹
+
         private void OnEnable()
         {
             isLoadScene = false;
+        }
+
+        public void ActiveLoginPanel()
+        {
+
         }
 
         private void Update()
@@ -19,6 +32,19 @@ namespace Jc
             {
                 isLoadScene = true;
                 Manager.Scene.LoadScene(SceneManager.SceneType.Main);
+            }
+        }
+
+        IEnumerator LoadSceneRoutine()
+        {
+            float time = loadTime;
+            yield return null;
+
+            while(time > 0)
+            {
+                time -= Time.deltaTime;
+
+                yield return null;
             }
         }
     }
