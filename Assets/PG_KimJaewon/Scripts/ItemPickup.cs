614ab42589321702345b28a0746813c54a057bd2
bbiyakbbiyak <wonwon1028@naver.com>
2024-05-22 11:37:51 +0900

    feat : player 네트워크 동기화

--- Assets/PG_KimJaewon/Scripts/ItemPickup.cs
+++ Assets/PG_KimJaewon/Scripts/ItemPickup.cs
@@ -2,59 +2,61 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ItemPickup : MonoBehaviour
+namespace KIMJAEWON
 {
-    private GameObject currentItem; // 현재 들고 있는 아이템을 추적
-    public Transform dropPosition; // 아이템을 내려놓을 위치
-
-    private void Update()
+    public class ItemPickup : MonoBehaviour
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        private GameObject currentItem; // 현재 들고 있는 아이템을 추적
+        public Transform dropPosition; // 아이템을 내려놓을 위치
+
+        private void Update()
         {
-            // 현재 아이템을 들고 있지 않다면
-            if (currentItem == null)
+            if (Input.GetKeyUp(KeyCode.Space))
             {
-                Debug.Log("아이템을 집었다 !");
-                PickupPossible(); // 아이템을 들 수 있는지 확인하고 들기
-            }
-            else
-            {
-                Debug.Log("아이템을 내려놓았다 !");
-                DropDownPossible(); // 아이템을 내려놓기
+                // 현재 아이템을 들고 있지 않다면
+                if (currentItem == null)
+                {
+                    Debug.Log("아이템을 집었다 !");
+                    PickupPossible(); // 아이템을 들 수 있는지 확인하고 들기
+                }
+                else
+                {
+                    Debug.Log("아이템을 내려놓았다 !");
+                    DropDownPossible(); // 아이템을 내려놓기
+                }
             }
         }
-    }
 
-    public void Pickup(Transform player)
-    {
-        currentItem = gameObject; // 현재 게임 오브젝트를 currentItem으로 설정
-        transform.SetParent(player); // 아이템의 부모를 플레이어로 설정
-       // Pickup(Collider.transform); // 아이템 위치를 플레이어 위치로 설정
-        GetComponent<Collider>().enabled = false; // 아이템의 Collider를 비활성화
-        GetComponent<Rigidbody>().isKinematic = true;
-        Debug.Log("아이템을 획득하였습니다");
-    }
-
-    private void PickupPossible()
-    {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
-        foreach (Collider collider in colliders)
+        public void Pickup(Transform player)
         {
-            if (collider.CompareTag("Player"))
+            currentItem = gameObject; // 현재 게임 오브젝트를 currentItem으로 설정
+            transform.SetParent(player); // 아이템의 부모를 플레이어로 설정
+                                         // Pickup(Collider.transform); // 아이템 위치를 플레이어 위치로 설정
+            GetComponent<Collider>().enabled = false; // 아이템의 Collider를 비활성화
+            GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log("아이템을 획득하였습니다");
+        }
+
+        private void PickupPossible()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
+            foreach (Collider collider in colliders)
             {
-                Pickup(collider.transform);
-                return;
+                if (collider.CompareTag("Player"))
+                {
+                    Pickup(collider.transform);
+                    return;
+                }
             }
         }
-    }
- 
-    private void DropDownPossible()
-    {
-        currentItem.transform.SetParent(null); // 아이템의 부모를 해제
-        //currentItem.transform.position = dropPosition.position; // 지정된 위치에 아이템을 배치
-        currentItem.GetComponent<Collider>().enabled = true; // 아이템의 Collider를 활성화
-        currentItem.GetComponent<Rigidbody>().isKinematic = false;
-        currentItem = null; 
+
+        private void DropDownPossible()
+        {
+            currentItem.transform.SetParent(null); // 아이템의 부모를 해제
+                                                   //currentItem.transform.position = dropPosition.position; // 지정된 위치에 아이템을 배치
+            currentItem.GetComponent<Collider>().enabled = true; // 아이템의 Collider를 활성화
+            currentItem.GetComponent<Rigidbody>().isKinematic = false;
+            currentItem = null;
+        }
     }
 }
