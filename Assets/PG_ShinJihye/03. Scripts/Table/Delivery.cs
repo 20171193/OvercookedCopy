401b0d02988677d0de20afa9e51a1302557f1f9f
JiHye Shin <werytuiop6@gmail.com>
2024-05-23 14:46:42 +0900

    feat : Delivery 스크립트 추가

--- Assets/PG_ShinJihye/03. Scripts/Table/Delivery.cs
+++ Assets/PG_ShinJihye/03. Scripts/Table/Delivery.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Delivery : Table
 {
+    [SerializeField] int finalScore;
+    [SerializeField] int addScore;
+
+    private void OnEnable()
+    {
+        finalScore = 0;
+        addScore = 5;
+    }
 
     public override void PutDownItem(Item item)
     {
-        base.PutDownItem(item);  // 제출대에 내려놓고
+        if (item.Type != ItemType.Plate)
+        {
+            Debug.Log("접시가 필요합니다.");
+            return;
+        }
+        Debug.Log("1");
+        base.PutDownItem(item);
+        Debug.Log("1");
 
         Debug.Log(item.name);
+        StartCoroutine(DeliveryMenu(item));
+    }
+
+    IEnumerator DeliveryMenu(Item item)
+    {
+        finalScore += addScore;
+        // UI로 받은 점수 띄워주기
+
+        yield return new WaitForSeconds(1.0f);
         Destroy(item.gameObject);
+        Debug.Log("제출완료");
     }
 }
