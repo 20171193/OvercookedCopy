04e85e08b680cc08ddda8722e002212c7322dd2f
임준희 <imjoonhi@gmail.com>
2024-05-23 13:11:46 +0900

    feat : Pan 네트워크 실험

--- Assets/PG_ImJoonhi/02. Scripts/Table/Ingredient Create.cs
+++ Assets/PG_ImJoonhi/02. Scripts/Table/Ingredient Create.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using UnityEngine;
 
 namespace JH
@@ -5,16 +6,25 @@ namespace JH
     public class IngredientCreate : MonoBehaviour
     {
         [Header("Prefabs")]
-        [SerializeField] IngredientsObject ingredientObject;    // IngredientsObject 속성을 가진 Prefab
+        [SerializeField] GameObject ingredientObject;    // IngredientsObject 속성을 가진 Prefab
 
+        [PunRPC]
         /// <summary>재료를 생성 및 부여하는 함수</summary>
         /// <param name="GeneratePoint">GeneratePoint는 생성한 재료 GameObject를 자식으로 넣을 위치에 대한 인수입니다.</param>
         void TakeIngredient(GameObject GeneratePoint)
         {
-            IngredientsObject ingObj = Instantiate(ingredientObject, GeneratePoint.transform.position, GeneratePoint.transform.rotation);
+            GameObject ingObj = PhotonNetwork.Instantiate($"Ingredient Prefab/{ingredientObject.name}", GeneratePoint.transform.position, GeneratePoint.transform.rotation);
             ingObj.transform.SetParent(GeneratePoint.transform);
         }
 
+        [PunRPC]
+        void SetGenPoint()
+        {
+
+        }
+
+
+            
         #region Debug
 #if UNITY_EDITOR
         [Header("Debug")]
@@ -24,7 +34,10 @@ namespace JH
         public void DebugTakeOut()
         {
             if (DebugGenpoint != null)
+            {
+                PhotonView photonView = PhotonView.Get(this);
                 TakeIngredient(DebugGenpoint);
+            }
         }
 #endif
         #endregion
