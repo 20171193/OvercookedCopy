1924bd28f68b77dbdab72781e87e1f7fbbbfcc21
JiHye Shin <werytuiop6@gmail.com>
2024-05-23 13:56:44 +0900

    feat : AirballoonScene 수정

--- Assets/PG_ShinJihye/03. Scripts/Table/Delivery.cs
+++ Assets/PG_ShinJihye/03. Scripts/Table/Delivery.cs
@@ -4,4 +4,12 @@ using UnityEngine;
 
 public class Delivery : Table
 {
+
+    public override void PutDownItem(Item item)
+    {
+        base.PutDownItem(item);  // 제출대에 내려놓고
+
+        Debug.Log(item.name);
+        Destroy(item.gameObject);
+    }
 }
