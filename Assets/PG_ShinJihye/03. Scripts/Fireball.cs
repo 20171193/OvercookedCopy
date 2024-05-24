451755f93376ce30427b7e8d7b31e346f18bb7c6
bbiyakbbiyak <wonwon1028@naver.com>
2024-05-24 10:32:11 +0900

    feat : 불 기믹 랜덤생성요소 추가

--- Assets/PG_ShinJihye/03. Scripts/Fireball.cs
+++ Assets/PG_ShinJihye/03. Scripts/Fireball.cs
@@ -4,47 +4,51 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Fireball : MonoBehaviour
+
+namespace KJW
 {
-    [SerializeField] Vector3 targetPos;
-    [SerializeField] float throwTime;
-    private Collider collider;
-    private void Awake()
+    public class Fireball : MonoBehaviour
     {
-        collider = GetComponent<Collider>();
-        collider.enabled = false;
-    }
-
-    public void SetTargetPos(Vector3 position)
-    {
-        targetPos = position;
-        StartCoroutine(ThrowFireballRoutine());
-    }
-
-    IEnumerator ThrowFireballRoutine()
-    {
-        Vector3 startPoint = transform.position;
-        Vector3 endPoint = targetPos;
-
-        float xSpeed = (endPoint.x - startPoint.x) / throwTime;
-        float zSpeed = (endPoint.z - startPoint.z) / throwTime;
-        float ySpeed = -1 * (0.5f * Physics.gravity.y * throwTime * throwTime + startPoint.y) / throwTime;
-
-        float curTime = 0f;
-        while (curTime < throwTime)
+        [SerializeField] Vector3 targetPos;
+        [SerializeField] float throwTime;
+        private Collider collider;
+        private void Awake()
         {
-            curTime += Time.deltaTime;
-
-            transform.position += new Vector3(xSpeed, ySpeed, zSpeed) * Time.deltaTime;
-            ySpeed += Physics.gravity.y * Time.deltaTime;
-
-            yield return null;
+            collider = GetComponent<Collider>();
+            collider.enabled = false;
         }
-        
-        transform.position = endPoint;
-        collider.enabled = true;
-        yield return null;
 
-        // 떨어지고 나서 할 일
+        public void SetTargetPos(Vector3 position)
+        {
+            targetPos = position;
+            StartCoroutine(ThrowFireballRoutine());
+        }
+
+        IEnumerator ThrowFireballRoutine()
+        {
+            Vector3 startPoint = transform.position;
+            Vector3 endPoint = targetPos;
+
+            float xSpeed = (endPoint.x - startPoint.x) / throwTime;
+            float zSpeed = (endPoint.z - startPoint.z) / throwTime;
+            float ySpeed = -1 * (0.5f * Physics.gravity.y * throwTime * throwTime + startPoint.y) / throwTime;
+
+            float curTime = 0f;
+            while (curTime < throwTime)
+            {
+                curTime += Time.deltaTime;
+
+                transform.position += new Vector3(xSpeed, ySpeed, zSpeed) * Time.deltaTime;
+                ySpeed += Physics.gravity.y * Time.deltaTime;
+
+                yield return null;
+            }
+
+            transform.position = endPoint;
+            collider.enabled = true;
+            yield return null;
+
+            // 떨어지고 나서 할 일
+        }
     }
 }
