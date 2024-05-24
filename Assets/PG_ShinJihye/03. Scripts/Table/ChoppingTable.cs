c07abf88eb98b93adb27a40f199188ef913ddbec
JiHye Shin <werytuiop6@gmail.com>
2024-05-24 13:39:27 +0900

    feat : 도마 작업중

--- Assets/PG_ShinJihye/03. Scripts/Table/ChoppingTable.cs
+++ Assets/PG_ShinJihye/03. Scripts/Table/ChoppingTable.cs
@@ -6,10 +6,42 @@ public class ChoppingTable : Table
 {
     [SerializeField] float choppingTime; // 6번
     [SerializeField] float currentTime;
+    [SerializeField] float choppingSpeed;
 
+    [SerializeField] BillBoard choppingBar;
 
-    public void ChoppingIngredient()
+    private void OnEnable()
     {
+        if (placedItem != null)
+        {
+            choppingBar = placedItem.GetComponentInChildren<BillBoard>();
+        }
+    }
+
+    public override bool PutDownItem(Item item)
+    {
+        return base.PutDownItem(item);
+    }
+
+    public override Item PickUpItem()
+    {
+        return base.PickUpItem();
+    }
+
+    public override void Interactable()
+    {
+        if (!PutDownItem())
+            return;
+
+        //애니메이션 실행하고
+        choppingBar.gameObject.SetActive(true);
 
     }
+
+    public void ChopIngredient()
+    {
+        
+    }
+
+    
 }
