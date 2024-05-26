af1e75f564da3e5c033c0a79e0cb0682a8ce1358
심재천 <shimnaldo98@gmail.com>
2024-05-27 00:09:43 +0900

    feat : 캠페인 씬 오디오 리소스적용 및 카메라 연출 추가

--- Assets/Common/01. Scripts/Util/Extension.cs
+++ Assets/Common/01. Scripts/Util/Extension.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 public static class Extension
 {
     // 레이어마스크가 해당 레이어를 포함하고 있는지 체크
     public static bool Contain(this LayerMask layerMask, int layer)
     {
         return ((1 << layer) & layerMask) != 0;
+    }
+
+    // 일정시간 딜레이 후 Action
+    public static IEnumerator ActionDelay(float delayTime, UnityAction action)
+    {
+        yield return new WaitForSeconds(delayTime);
+        action?.Invoke();
     }
 }
 
