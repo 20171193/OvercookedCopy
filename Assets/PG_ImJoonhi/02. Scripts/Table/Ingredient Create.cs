ce35276a5410ff94db835e15c7e9be94b4d9968d
임준희 <imjoonhi@gmail.com>
2024-05-27 17:09:51 +0900

    fix : 아이템 동기화 횟수 증가 및 ingredient Box 플레이어 아이템 변경 안되던것 수정

--- Assets/PG_ImJoonhi/02. Scripts/Table/Ingredient Create.cs
+++ Assets/PG_ImJoonhi/02. Scripts/Table/Ingredient Create.cs
@@ -10,14 +10,15 @@ namespace JH
         [SerializeField] GameObject ingredientObject;    // IngredientsObject 속성을 가진 Prefab
 
         [PunRPC]
-        public void GenIngredient(int PlayerHoldPointPhotonID)
+        public void GenIngredient(int PlayerHoldPointPhotonID, int ingredient)
         {
             Transform HoldPoint = PhotonView.Find(PlayerHoldPointPhotonID).gameObject.transform;
-            GameObject ingObj = PhotonNetwork.Instantiate($"Ingredient_Create/{ingredientObject.name}", HoldPoint.position, HoldPoint.rotation);
-            Debug.Log($"IngObj : {HoldPoint.gameObject.name}");
-            Debug.Log($"IngObj : {ingObj.name}");
+            GameObject ingObj = PhotonView.Find(ingredient).gameObject;
+            // GameObject ingObj = PhotonNetwork.Instantiate($"Ingredient_Create/{ingredientObject.name}", HoldPoint.position, HoldPoint.rotation);
+            // Debug.Log($"IngObj : {HoldPoint.gameObject.name}");
+            // Debug.Log($"IngObj : {ingObj.name}");
             ingObj.GetComponent<IngredientsObject>().GoTo(HoldPoint.gameObject);
-            HoldPoint.gameObject.transform.parent.parent.parent.gameObject.GetComponent<PlayerAction>().SetPickedItem(ingObj.GetComponent<IngredientsObject>());
+            // HoldPoint.gameObject.transform.parent.parent.parent.gameObject.GetComponent<PlayerAction>().SetPickedItem(ingObj.GetComponent<IngredientsObject>());
         }
 
         /// <summary>재료를 생성 및 부여하는 함수</summary>
@@ -25,7 +26,9 @@ namespace JH
         public void TakeIngredient(GameObject GeneratePoint)
         {
             PhotonView photonView = PhotonView.Get(this);
-            photonView.RPC("GenIngredient", RpcTarget.MasterClient, GeneratePoint.GetPhotonView().ViewID);
+            GameObject ingObj = PhotonNetwork.Instantiate($"Ingredient_Create/{ingredientObject.name}", GeneratePoint.transform.position, GeneratePoint.transform.rotation);
+            photonView.RPC("GenIngredient", RpcTarget.MasterClient, GeneratePoint.GetPhotonView().ViewID, ingObj.GetPhotonView().ViewID);
+            GeneratePoint.gameObject.transform.parent.parent.parent.gameObject.GetComponent<PlayerAction>().SetPickedItem(ingObj.GetComponent<IngredientsObject>());
         }
 
 
